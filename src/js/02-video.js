1c3e1ca8d3654e8417e93167b5f558dcebe0252a
lfurmanchuk <lesyazhuruk@gmail.com>
2022-11-27 23:17:06 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+  player.on("timeupdate", throttle(onPlay, 1000));
+
+setCurrentTime();
+
+function onPlay(evt) {
+  localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+}
+
+function setCurrentTime() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+  }
+}
