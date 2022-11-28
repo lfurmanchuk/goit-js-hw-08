75a51b75aedf1259a4e8a4e7490a756224cad6bf
lfurmanchuk <lesyazhuruk@gmail.com>
2022-11-28 02:01:35 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+populateInput();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onInput(e) {
+const {
+    email,
+    message
+} = e.currentTarget.elements;
+
+ const formData = {
+    email: email.value,
+    message: message.value,
+    };
+
+    localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(formData));
+}
+
+function populateInput(e) {
+    const savedData = JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY'));
+
+    if (savedData) {
+        console.log(savedData);
+        email.value = savedData.email;
+        message.value = savedData.message;
+    }
+}
