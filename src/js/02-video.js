3a111e6722a3e08fec075970468120dd38fbf7d8
lfurmanchuk <lesyazhuruk@gmail.com>
2022-11-28 20:00:01 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,3 +19,28 @@ function setCurrentTime() {
     player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
   }
 }
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// function onTimeUpdate(data) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+// }
+
+// const currentTimeVideo = localStorage.getItem(LOCALSTORAGE_KEY);
+
+// player.setCurrentTime(currentTimeVideo, function (seconds) {
+//   seconds = currentTimeVideo;
+// });
+
